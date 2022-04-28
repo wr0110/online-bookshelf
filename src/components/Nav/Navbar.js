53dc0fb09fe718e38b675515ad0353fc99ed3b87
wr0110 <wittyraccoon0110@gmail.com>
2022-04-28 12:28:39 -0500

    Added showMenu functionality in navbar

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -1,43 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "./Navbar.module.css";
 import { BiSearch } from "react-icons/bi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import Button from "../button/Button";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu((prevState) => !prevState);
+  };
+
   return (
     <nav>
       <p className={styled.logo}>Bookmark</p>
 
-      <div className={styled.menu}>
-        <ul className={styled.navlinks}>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">Explore</a>
-          </li>
-          <li>
-            <a href="/">Genres</a>
-          </li>
-          <li>
-            <a href="/">Library</a>
-          </li>
-        </ul>
+      {showMenu && (
+        <div className={styled.menu}>
+          <ul className={styled.navlinks}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Explore</a>
+            </li>
+            <li>
+              <a href="/">Genres</a>
+            </li>
+            <li>
+              <a href="/">Library</a>
+            </li>
+          </ul>
 
-        <div className={styled.btngroup}>
-          <form>
-            <label htmlFor="search" className={styled["search-icon"]}>
-              <BiSearch size="20px" color="#c2a410" />
-            </label>
-            <input type="text" name="search" placeholder="Search" />
-          </form>
+          <div className={styled.btngroup}>
+            <form>
+              <label htmlFor="search" className={styled["search-icon"]}>
+                <BiSearch size="20px" color="#c2a410" />
+              </label>
+              <input type="text" name="search" placeholder="Search" />
+            </form>
 
-          <Button>Login</Button>
+            <Button>Sign In</Button>
+          </div>
         </div>
-      </div>
+      )}
 
-      <div>
+      <div onClick={handleMenu} className={styled["menu-icon"]}>
         <GiHamburgerMenu size="25px" color="#c2a410" />
       </div>
     </nav>
