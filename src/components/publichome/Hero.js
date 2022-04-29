67389b735501887e1169c27af1ad9d7c13fb5b4b
wr0110 <wittyraccoon0110@gmail.com>
2022-04-29 13:25:34 -0500

    Finished hero component

--- src/components/publichome/Hero.js
+++ src/components/publichome/Hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Heading from "../../ui/heading/Heading";
 import Container from "../../ui/wrapper/Container";
+import Button from "../button/Button";
 import styled from "./Hero.module.css";
 
 const Hero = () => {
@@ -16,6 +17,14 @@ const Hero = () => {
               </>
             }
           />
+
+          <p className={styled["hero-intro"]}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus
+            velit senectus in nunc ut dictum aliquam id platea. In eget amet,
+            imperdiet tellus. Sit sit orci in eu. Quis pellentesque.
+          </p>
+
+          <Button>Start Organizing</Button>
         </article>
       </Container>
     </section>
