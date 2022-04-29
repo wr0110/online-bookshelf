d907ad2513b266f56d41fbd5b8310b42a19250b4
wr0110 <wittyraccoon0110@gmail.com>
2022-04-28 22:47:59 -0500

    Added comments and fixed styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,23 @@ import React, { useEffect, useRef, useState } from "react";
 import styled from "./Nav.module.css";
 import { BiSearch } from "react-icons/bi";
 import Button from "./button/Button";
+
 const Nav = () => {
   const [width, setwidth] = useState(0);
   const [showSearch, setshowSearch] = useState(false);
-  const ulref = useRef();
+  const ulRef = useRef();
 
+  // sets the current width of the screen
   const widthfunction = () => {
     setwidth(window.innerWidth);
   };
 
+  /**
+   * add event listener to the window
+   * checks if the current width >= 768
+   * if width >= 768, set the state to the opposite of what it currently is
+   * use the cleanup function to remove the eventlistener
+   */
   useEffect(() => {
     window.addEventListener("resize", widthfunction);
     if (showSearch && width >= 768) {
@@ -19,60 +27,64 @@ const Nav = () => {
     return () => window.removeEventListener("resize", widthfunction);
   }, [width, showSearch]);
 
+  //!todo : change pageyoffset to scrollx
   useEffect(() => {
     if (width <= 768) {
       let prevScrollpos = window.pageYOffset;
       window.onscroll = function () {
         let currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
-          ulref.current.style.top = "70px";
+          ulRef.current.style.top = "70px";
         } else {
-          ulref.current.style.top = "-80px";
+          ulRef.current.style.top = "-80px";
         }
         prevScrollpos = currentScrollPos;
       };
     }
   }, [width]);
 
-  const handlesearchbar = () => {
+  //set the state when the user clicks on the search icon
+  const handleSearch = () => {
     setshowSearch((state) => !state);
   };
 
   return (
-    <>
-      <nav className={styled.navbar}>
-        <div className={styled.searchicon} onClick={handlesearchbar}>
-          <BiSearch size="20px" />
-        </div>
-        <p className={styled.logo}>Bookmark</p>
-        <Button className={styled.btnlog}> Sign In</Button>
-        <ul className={styled.navitems} ref={ulref}>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">Explore</a>
-          </li>
-          <li>
-            <a href="/">Genres</a>
-          </li>
-          <li>
-            <a href="/">Library</a>
-          </li>
-        </ul>
+    <nav className={styled.navbar}>
+      <div className={styled["search-icon"]} onClick={handleSearch}>
+        <BiSearch size="25px" color="#c2a410" />
+      </div>
 
-        <div
-          id="search"
-          className={
-            showSearch ? `${styled.searchbar}` : `${styled.nosearchbar}`
-          }
-        >
-          <form>
-            <input type="text" />
-          </form>
-        </div>
-      </nav>
-    </>
+      <p className={styled.logo}>BookMark</p>
+
+      <Button className={styled.btnlog}> Sign In</Button>
+
+      {/*use Link for li */}
+      <ul className={styled["nav-items"]} ref={ulRef}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Explore</a>
+        </li>
+        <li>
+          <a href="/">Genres</a>
+        </li>
+        <li>
+          <a href="/">Library</a>
+        </li>
+      </ul>
+
+      {/* if showSearch is true apply the appropriate class */}
+      <div
+        className={
+          showSearch ? `${styled.searchbar}` : `${styled["desktop-search-bar"]}`
+        }
+      >
+        <form>
+          <input type="text" placeholder="Search" />
+        </form>
+      </div>
+    </nav>
   );
 };
 
