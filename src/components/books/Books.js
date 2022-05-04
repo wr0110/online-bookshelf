e608a04639a4f13fcd3b42f140a2d58568d1251c
wr0110 <wittyraccoon0110@gmail.com>
2022-05-04 11:33:20 -0500

    Fetched data from api

---
+++ src/components/books/Books.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Books = ({ book }) => {
+  return <section>Books</section>;
+};
+
+export default Books;
--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import Books from "../books/Books";
+import Container from "../../helpers/wrapper/Container";
+import Modal from "../../helpers/modal/Modal";
+import Loading from "../../helpers/Loading";
 
 const SearchResults = () => {
-  //the location object returns information about the page.
+  const [bookResults, setBookResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  /** the location object returns information about the page
+   * convert the location object into a javascript object
+   * get the search property and store is value in a constant  */
   const location = useLocation();
-
-  //  convert the location object into a javascript object
   const queryParams = new URLSearchParams(location.search);
+  const searchQuery = queryParams.get("search");
 
-  //get the search property and store is value in a constant
-  const search = queryParams.get("search");
+  /**
+   * url includes searchQuery, orderBy relevance and maxResults 0f 40
+   * setBookResults to the results from the fetched data
+   */
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+    const url = ` https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&orderBy=relevance&maxResults=40&key=${apiKey}`;
 
-  if (!search) {
+    const fetchBook = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setBookResults(data.items);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+
+    fetchBook();
+  }, [searchQuery]);
+
+  const allBooks = bookResults.map((book) => (
+    <Books key={book.id} book={book} />
+  ));
+
+  console.log(bookResults);
+
+  /**prevent user from visiting the route manually route,
+   * the user can only visit this page if there is a search term*/
+  if (!searchQuery) {
     return <Navigate to="/" />;
   }
 
-  return <div>{search}</div>;
+  return (
+    <section>
+      <Container>
+        {loading ? (
+          <Modal>
+            <Loading />{" "}
+          </Modal>
+        ) : (
+          allBooks
+        )}
+      </Container>
+    </section>
+  );
 };
 
 export default SearchResults;
