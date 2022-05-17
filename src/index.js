c21adf9882e9088a94d3b4fd24a477c4c0612652
wr0110 <wittyraccoon0110@gmail.com>
2022-05-17 11:54:00 -0500

    Added context to manage library state

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthContextProvider from "./contexts/authContext";
+import LibraryContextProvider from "./contexts/libraryContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <LibraryContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LibraryContextProvider>
   </AuthContextProvider>
 );
