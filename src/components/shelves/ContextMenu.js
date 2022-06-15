76581bf43cdc25d73c4850f5035271d6df1fc53b
wr0110 <wittyraccoon0110@gmail.com>
2022-06-15 13:50:18 -0500

    Styled context menu

--- src/components/shelves/ContextMenu.js
+++ src/components/shelves/ContextMenu.js
@@ -1,11 +1,17 @@
 import React from "react";
+import styled from "./ContextMenu.module.css";
 
 const ContextMenu = (props) => {
   console.log(props.selectedShelf);
+
+  const menuStyle = {
+    top: props.positions.top + 20 + "px",
+    left: props.positions.left - 40 + "px",
+  };
   return (
-    <section>
-      <p>Edit</p>
-      <p>Delete</p>
+    <section className={styled["context-menu"]} style={menuStyle}>
+      <p>Rename Shelf</p>
+      <p>Delete Shelf</p>
     </section>
   );
 };
