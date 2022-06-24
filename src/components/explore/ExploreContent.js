6b4d34617f0e81b59aa1eb8388f36553445572e8
wr0110 <wittyraccoon0110@gmail.com>
2022-06-23 21:05:37 -0500

    Started displaying books based on subject

--- src/components/explore/ExploreContent.js
+++ src/components/explore/ExploreContent.js
@@ -1,16 +1,81 @@
 import React from "react";
+import styled from "./ExploreContent.module.css";
 import useExplore from "../../hooks/useExplore";
 
 const ExploreContent = () => {
-  const [loadingFictionBooks, fictionBooks] = useExplore("Fiction");
-
-  return (
-    <section>
-      <div>
-        {loadingFictionBooks ? "Getting Fiction Books..." : fictionBooks}
-      </div>
-    </section>
+  //custom hook to fetch books
+  const [loadingFictionBooks, fictionBooks] = useExplore("", "fiction");
+  const [loadingRomanceBooks, romanceBooks] = useExplore("", "romance");
+  const [loadingLoveBooks, loveBooks] = useExplore("", "love");
+  const [loadingFantasyBooks, fantasyBooks] = useExplore("", "fantasy");
+  const [loadingMysteryBooks, mysteryBooks] = useExplore("", "mystery");
+  const [loadingHorrorBooks, horrorBooks] = useExplore("", "horror");
+  const [loadingSciFiBooks, sciFiBooks] = useExplore("", "science fiction");
+  const [loadingThrillerBooks, thrillerBooks] = useExplore("", "thriller");
+  const [loadingContemporaryBooks, contemporaryBooks] = useExplore(
+    "",
+    "contemporary"
   );
+
+  const contents = [
+    {
+      id: 1,
+      title: "Fiction",
+      books: fictionBooks,
+      loading: loadingFictionBooks,
+    },
+    {
+      id: 2,
+      title: "Romance",
+      books: romanceBooks,
+      loading: loadingRomanceBooks,
+    },
+    { id: 3, title: "Love", books: loveBooks, loading: loadingLoveBooks },
+    {
+      id: 4,
+      title: "Fantasy",
+      books: fantasyBooks,
+      loading: loadingFantasyBooks,
+    },
+    {
+      id: 5,
+      title: "Mystery",
+      books: mysteryBooks,
+      loading: loadingMysteryBooks,
+    },
+    { id: 6, title: "Horror", books: horrorBooks, loading: loadingHorrorBooks },
+    { id: 7, title: "Sci-Fi", books: sciFiBooks, loading: loadingSciFiBooks },
+    {
+      id: 8,
+      title: "Thriller",
+      books: thrillerBooks,
+      loading: loadingThrillerBooks,
+    },
+
+    {
+      id: 9,
+      title: "Contemporary",
+      books: contemporaryBooks,
+      loading: loadingContemporaryBooks,
+    },
+  ];
+
+  const renderContents = contents.map((content, i) => {
+    return (
+      <div key={Date.now() + i}>
+        {content.loading && "Getting  Books..."}
+
+        {!content.loading && (
+          <section>
+            <h2>{content.title}</h2>
+            <div className={styled["explore-books"]}>{content.books}</div>
+          </section>
+        )}
+      </div>
+    );
+  });
+
+  return <section>{renderContents}</section>;
 };
 
 export default ExploreContent;
--- src/hooks/useExplore.js
+++ src/hooks/useExplore.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import Books from "../components/books/Books";
 import LibraryActions from "../components/books/LibraryActions";
 
-const useExplore = (subject) => {
+const useExplore = (query, subject) => {
   const [loading, setLoading] = useState(false);
   const [exploreBooks, setExploreBooks] = useState([]);
 
   //fetch the books from the API based on the subject
   useEffect(() => {
     const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
-    const url = `https://www.googleapis.com/books/v1/volumes?q=subject:${subject}&maxResults=20&key=${apiKey}`;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}subject:${subject}&maxResults=20&key=${apiKey}`;
 
     const fetchBook = async () => {
       setLoading(true);
@@ -23,17 +23,28 @@ const useExplore = (subject) => {
       setLoading(false);
     };
     fetchBook();
-  }, [subject]);
+  }, [query, subject]);
 
-  //map over the bookResults array and return a Book with the info
-  const allBooks = exploreBooks.map((book) => {
-    const { id, searchInfo } = book;
-    const { title, authors, publishedDate, categories, imageLinks } =
-      book.volumeInfo;
+  /**map over the bookResults array and return a Book with the info
+     destucture properties from the book object and pass them as props
+     substitute the searchInfo object with the description property
+     calculate the key because some books will be repeated
+     */
+
+  const allBooks = exploreBooks?.map((book, i) => {
+    const { id } = book;
+    const {
+      title,
+      authors,
+      publishedDate,
+      categories,
+      imageLinks,
+      description,
+    } = book.volumeInfo;
 
     const bookData = {
       id,
-      searchInfo,
+      searchInfo: { textSnippet: description },
       title,
       authors,
       publishedDate,
@@ -42,7 +53,7 @@ const useExplore = (subject) => {
     };
     return (
       <Books
-        key={book.id}
+        key={Date.now() + i}
         book={bookData}
         actionsComponent={<LibraryActions book={bookData} />}
       />
