caeb293298c283785210c4cc2ea405e32b7e4090
wr0110 <wittyraccoon0110@gmail.com>
2022-06-25 12:23:50 -0500

    Scrolled to top on route change

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -4,6 +4,7 @@ import BookDetails from "../components/books/BookDetails";
 import Nav from "../components/nav/Nav";
 import SearchResults from "../components/search/SearchResults";
 import ProtectedRoute from "../helpers/ProtectedRoute";
+import ScrollToTop from "../helpers/ScrollToTop";
 import Explore from "./Explore";
 import Library from "./Library";
 import PublicHome from "./PublicHome";
@@ -13,30 +14,32 @@ const Pages = () => {
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path="/" exact element={<PublicHome />} />
-        <Route path="/explore" element={<Explore />} />
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" exact element={<PublicHome />} />
+          <Route path="/explore" element={<Explore />} />
 
-        <Route
-          path="/library"
-          element={
-            <ProtectedRoute>
-              <Library />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/shelves"
-          element={
-            <ProtectedRoute>
-              <Shelves />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/library"
+            element={
+              <ProtectedRoute>
+                <Library />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/shelves"
+            element={
+              <ProtectedRoute>
+                <Shelves />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route path="/results" element={<SearchResults />} />
-        <Route path="results/:bookId" element={<BookDetails />} />
-      </Routes>
+          <Route path="/results" element={<SearchResults />} />
+          <Route path="results/:bookId" element={<BookDetails />} />
+        </Routes>
+      </ScrollToTop>
     </>
   );
 };
