5a520d0f0bd6a9bbe740f2c61e5a71f5b7b66b88
wr0110 <wittyraccoon0110@gmail.com>
2022-07-04 17:36:35 -0500

    Added new routes

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -7,7 +7,10 @@ import SearchResults from "../components/search/SearchResults";
 import { AuthContext } from "../contexts/authContext";
 import NoMatch from "../helpers/routes/NoMatch";
 import ProtectedRoute from "../helpers/routes/ProtectedRoute";
+import About from "./About";
 import Explore from "./Explore";
+import Faq from "./Faq";
+import Instructions from "./Instructions";
 import Library from "./Library";
 import PrivateHome from "./PrivateHome";
 import PublicHome from "./PublicHome";
@@ -48,6 +51,11 @@ const Pages = () => {
 
         <Route path="/results" element={<SearchResults />} />
         <Route path="results/:bookId" element={<BookDetails />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/faqs" element={<Faq />} />
+        <Route path="/instructions" element={<Instructions />} />
+
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
